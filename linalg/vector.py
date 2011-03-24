06656481f3095bd03d7fde8371d7663f4f82433b
Renaud Blanch <blanch@imag.fr>
2011-03-24 13:32:52 +0100

    merged from python2

--- linalg/vector.py
+++ linalg/vector.py
@@ -9,14 +9,17 @@ Licence: GPLv3 or higher <http://www.gnu.org/licenses/gpl.html>
 
 # imports ####################################################################
 
-from __builtin__ import sum as _sum
 from math import sqrt as _sqrt
-from itertools import izip_longest as _izip_longest
+_sum = sum
 
 
 # globals ####################################################################
 
-def _zip(*ps): return _izip_longest(*ps, fillvalue=0.)
+def _zip_zero_pad(*ps):
+	len_max = max(len(p) for p in ps)
+	ps = [tuple(p) + (0.,)*(len_max-len(p)) for p in ps]
+	for t in zip(*ps):
+		yield t
 
 _O = tuple()
 _3D = 3
@@ -26,13 +29,13 @@ _X, _Y, _Z = range(_3D)
 # vector #####################################################################
 
 def vector(p1=_O, p0=_O):
-	return tuple(p1i-p0i for (p1i, p0i) in _zip(p1, p0))
+	return tuple(p1i-p0i for (p1i, p0i) in _zip_zero_pad(p1, p0))
 
 def mul(a, v):
 	return tuple(a*vi for vi in v)
 
 def add(u, v):
-	return tuple(ui+vi for (ui, vi) in _zip(u, v))
+	return tuple(ui+vi for (ui, vi) in _zip_zero_pad(u, v))
 
 def sum(u=vector(), *vs):
 	for v in vs:
@@ -40,10 +43,10 @@ def sum(u=vector(), *vs):
 	return u
 
 def sub(u, v):
-	return tuple(ui-vi for (ui, vi) in _zip(u, v))
+	return tuple(ui-vi for (ui, vi) in _zip_zero_pad(u, v))
 
 def dot(u, v):
-	return _sum(ui*vi for (ui, vi) in _zip(u, v))
+	return _sum(ui*vi for (ui, vi) in _zip_zero_pad(u, v))
 
 def cross(u, v):
 	assert len(u) == len(v) == _3D
